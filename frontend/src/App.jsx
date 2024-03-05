e9f2f2dd0eaa9237131c40a5689632fc01c2c695
Shreyash Tidke <shreyashtidke751@gmail.com>
2024-03-05 23:03:26 +0530

    created category page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,11 @@ import ShopCategory from "./Pagess/ShopCategory";
 import Product from "./Pagess/Product";
 import Cart from "./Pagess/Cart";
 import LoginSignup from "./Pagess/LoginSignup";
+import Footer from "./Components/Footer/Footer";
+import men_banner from './assets/banner_mens.png'
+import women_banner from './assets/banner_women.png'
+import kids_banner from './assets/banner_kids.png'
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,15 +22,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="mens" />} />
-          <Route path="/womens" element={<ShopCategory category="womens" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
