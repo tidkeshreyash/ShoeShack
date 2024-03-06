132ea56b326d19a37251c72354db4a02c3eac549
Shreyash Tidke <shreyashtidke751@gmail.com>
2024-03-06 22:51:36 +0530

    created login page

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt="" />
-        <p>SHOPPER</p>
+        <p>ShoeShack</p>
       </div>
       <ul className='nav-menu'>
         <li onClick={()=>{setMenu("shop")}}> <Link style={{textDecoration: "none"}} to='/'>Shop </Link> {menu === "shop"? <hr />:<></>} </li>
