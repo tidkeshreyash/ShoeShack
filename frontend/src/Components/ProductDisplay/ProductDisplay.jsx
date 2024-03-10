1b4371a6e003ef654dc20f8933dfe95370570d48
Shreyash Tidke <shreyashtidke758@gmail.com>
2024-03-10 23:17:17 +0530

    completed the product display page

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -49,8 +49,7 @@ const ProductDisplay = (props) =>{
                     </div>
                 </div>
                 <button>ADD TO CART</button>
-                <p className="productdisplay-right-category"><span>Category : </span>Women, T-Shirt, Crop Top</p>
-                <p className="productdisplay-right-category"><span>Tags : </span>Modern, Latest</p>
+                <p className="productdisplay-right-category"><span>Category : </span>Women, T-Shirt, Crop Top <span>Tags : </span>Modern, Latest</p>
             </div>
         </div>
     )
